abcbfa7c2499f35c6a0889aab586bcfe2c00f23e
Laerositus <Laerositus@gmail.com>
2025-09-24 12:58:18 +0200

    Removed Hard Coded API Token

--- app/src/main/java/com/prodvx/prodvx_demo/api/ProDVXApi.kt
+++ app/src/main/java/com/prodvx/prodvx_demo/api/ProDVXApi.kt
@@ -10,12 +10,9 @@ private const val PROTOCOL = "http"
 private const val HOST = "localhost"
 private const val PORT = "3535"
 private const val VERSION = "v1"
-
 private const val API_URL = "${PROTOCOL}://${HOST}:${PORT}/${VERSION}"
 
-private const val DEFAULT_TOKEN = "MEYCIQCeykYnEnmf3pH0KmmwR5qxLI6H4b7ZgqKLlTZWK1xVJgIhAO6C1CuS1Fm5RC26c99u62bJAAoAEXKp1_m10HbvWRyQ"
-private const val EXTRA_TOKEN = ""
-private const val TOKEN = DEFAULT_TOKEN
+var TOKEN: String? = null
 
 suspend fun sendRequest(method: HttpMethod, endpoint: String, params: Map<String, String>? = null): HttpResponse? {
     try {
@@ -28,7 +25,7 @@ suspend fun sendRequest(method: HttpMethod, endpoint: String, params: Map<String
         }
         val response: HttpResponse = httpClient.request(url) {
             this.method = method
-            header(HttpHeaders.Authorization, "Bearer $TOKEN")
+            header(HttpHeaders.Authorization, "Bearer ${TOKEN!!}")
         }
         return response
 
@@ -40,4 +37,9 @@ suspend fun sendRequest(method: HttpMethod, endpoint: String, params: Map<String
 
 suspend fun sleepDevice(): HttpResponse? {
     return sendRequest(HttpMethod.Get, "/sleepDevice")
+}
+
+fun updateToken(newToken: String) {
+    println("Set new token: $newToken")
+    TOKEN = newToken
 }
